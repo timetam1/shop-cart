2bb5633988e38fea851328c1a268e4ae10d4bb10
timetam1 <tanuma321bre@gmail.com>
2017-08-13 23:22:41 +0900

    Create staff_list.php

---
+++ staff/staff_list.php
@@ -0,0 +1,32 @@
+<p>スタッフ画面</p>
+
+<?php
+$dsn='mysql:dbname=shop5;host=localhost;charset=utf8';
+$user='root';
+$password='root';
+$dbh = new PDO($dsn,$user,$password);
+
+$sql = 'SELECT code,name FROM staff WHERE 1';
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+print '<form action = "staff_branch.php" method = "post">';
+while(true){
+	$rec = $stmt->fetch(PDO::FETCH_ASSOC);
+	if($rec == false){
+		break;
+
+
+	}
+	
+	   print '<input type = "radio" name = "code" value = "'.$rec['code'].'">';
+		print $rec['name'];
+}
+
+print '<input type = "submit"  name = "add" value = "新規登録する">';
+print '<input type = "submit"  name = "edit" value = "編集する">';
+print '<input type = "submit"  name = "delete" value = "削除する">';
+print '<input type ="submit" name = "disp" value = "詳細を見る">';
+
+print '</form>';
+?>
