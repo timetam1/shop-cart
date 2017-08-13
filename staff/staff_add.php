4875232380f76ca342b972d6d15c5cf69592a6eb
timetam1 <tanuma321bre@gmail.com>
2017-08-13 23:18:02 +0900

    Create staff_add.php

---
+++ staff/staff_add.php
@@ -0,0 +1,12 @@
+<p>会員登録入会画面</p>
+<form action= "staff_add_check.php" method = "post">
+	<dl>
+		<dt>名前</dt>
+		<dd>
+			<input type = "text" name = "name">
+		</dd>
+		<dt>パスワード</dt>
+		<input type = "password" name = "pass">
+	</dl>
+	<input type = "submit" value ="送信する"　/>
+</form>
